b7489e6a2f345cd8e4ab8fef8cb4d1fab60e8236
Alexey Novikov <alekseeey@gmail.com>
2015-03-03 13:02:49 +0200

    completed user delete implementation

--- app/controllers/user/Delete.php
+++ app/controllers/user/Delete.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers\User;
 
-use \App\Components\Controller;
+use \App\Components\Controller,
+    \App\Models\User;
 
 class Delete extends Controller {
 
     public function run() {
+        $params = $this->router->getParams();
+        User::findFirst(['id' => $params['id']])
+            ->delete();
         return $this->response();
     }
 
