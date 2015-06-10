0372e245d029651ba7a1539677f159bf01263c59
Alexey Novikov <alekseeey@gmail.com>
2015-06-10 14:08:44 +0300

    fixed collection meta total

--- app/Bootstrap/Console.php
+++ app/Bootstrap/Console.php
@@ -2,8 +2,9 @@
 
 namespace App\Bootstrap;
 
-use \Phalcon\CLI\Console as BaseConsole,
-    \Phalcon\Di\FactoryDefault\Cli,
+use \Phalcon\Di\FactoryDefault\Cli,
+
+    \Rise\Bootstrap\Console as BaseConsole,
 
     \App\Config\Database;
 
@@ -14,96 +15,9 @@ use \Phalcon\CLI\Console as BaseConsole,
 class Console extends BaseConsole {
 
     /**
-     * @var array
-     */
-    private $rawArguments;
-
-    /**
-     * @var array
-     */
-    private $formattedArguments;
-
-    /**
-     * @return array
-     */
-    public function getRawArguments() {
-        return $this->rawArguments;
-    }
-
-    /**
-     * @param array $arguments
      * @return Console
      */
-    public function setRawArguments(array $arguments) {
-        $this->rawArguments = $arguments;
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getFormattedArguments() {
-        return $this->formattedArguments;
-    }
-
-    /**
-     * @param array $arguments
-     * @return Console
-     */
-    public function setFormattedArguments(array $arguments) {
-        $this->formattedArguments = $arguments;
-        return $this;
-    }
-
-    /**
-     * @param array $arguments
-     */
-    public function __construct(array $arguments = []) {
-        $this
-            ->setRawArguments($arguments)
-            ->createFormattedArguments();
-    }
-
-    public function go() {
-        $this
-            ->createDependencies()
-            ->handle($this->getFormattedArguments());
-    }
-
-    private function createFormattedArguments() {
-        $formattedArguments = [];
-
-        if (count($this->getRawArguments()) > 1) {
-            foreach($this->getRawArguments() as $index => $argument) {
-                if($index == 1) {
-                    $formattedArguments['task'] = $this->createHandlerName($argument);
-                } elseif($index == 2) {
-                    $formattedArguments['action'] = $argument;
-                } elseif($index >= 3) {
-                    $formattedArguments['params'][] = $argument;
-                }
-            }
-        } else {
-            $formattedArguments['task'] = $this->createHandlerName();
-        }
-
-        $this->setFormattedArguments($formattedArguments);
-    }
-
-    /**
-     * @param string $argument
-     * @return string
-     */
-    private function createHandlerName($argument = 'main') {
-        $className = ucfirst($argument);
-
-        return '\\App\\Tasks\\' . $className;
-    }
-
-    /**
-     * @return Console
-     */
-    private function createDependencies() {
+    public function createDependencies() {
         $dependency = new Cli();
         $dependency->set('db', function() {
             return Database::get();
---
+++ rise/Bootstrap/Console.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Rise\Bootstrap;
+
+use \Phalcon\CLI\Console as BaseConsole;
+
+/**
+ * Class Console
+ * @package App\Bootstrap
+ */
+abstract class Console extends BaseConsole {
+
+    /**
+     * @var array
+     */
+    private $rawArguments;
+
+    /**
+     * @var array
+     */
+    private $formattedArguments;
+
+    /**
+     * @return array
+     */
+    public function getRawArguments() {
+        return $this->rawArguments;
+    }
+
+    /**
+     * @param array $arguments
+     * @return Console
+     */
+    public function setRawArguments(array $arguments) {
+        $this->rawArguments = $arguments;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFormattedArguments() {
+        return $this->formattedArguments;
+    }
+
+    /**
+     * @param array $arguments
+     * @return Console
+     */
+    public function setFormattedArguments(array $arguments) {
+        $this->formattedArguments = $arguments;
+        return $this;
+    }
+
+    /**
+     * @param array $arguments
+     */
+    public function __construct(array $arguments = []) {
+        $this
+            ->setRawArguments($arguments)
+            ->createFormattedArguments();
+    }
+
+    public function go() {
+        $this
+            ->createDependencies()
+            ->handle($this->getFormattedArguments());
+    }
+
+    private function createFormattedArguments() {
+        $formattedArguments = [];
+
+        if (count($this->getRawArguments()) > 1) {
+            foreach($this->getRawArguments() as $index => $argument) {
+                if($index == 1) {
+                    $formattedArguments['task'] = $this->createHandlerName($argument);
+                } elseif($index == 2) {
+                    $formattedArguments['action'] = $argument;
+                } elseif($index >= 3) {
+                    $formattedArguments['params'][] = $argument;
+                }
+            }
+        } else {
+            $formattedArguments['task'] = $this->createHandlerName();
+        }
+
+        $this->setFormattedArguments($formattedArguments);
+    }
+
+    /**
+     * @param string $argument
+     * @return string
+     */
+    private function createHandlerName($argument = 'main') {
+        $className = ucfirst($argument);
+
+        return '\\App\\Tasks\\' . $className;
+    }
+
+    /**
+     * @return Console
+     */
+    abstract public function createDependencies();
+
+}
