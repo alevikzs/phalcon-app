3610f049be466dcbd2c67a4e7c920a25804b3e61
alexey novikov <alekseeey@gmail.com>
2015-07-21 01:54:16 +0300

    added password not changed test

--- app/Tests/Api/User/UpdateTest.php
+++ app/Tests/Api/User/UpdateTest.php
@@ -44,6 +44,31 @@ class UpdateTest extends ApiTestCase {
         $this->assertTrue($isValidPassword);
     }
 
+    public function testPasswordNotChange() {
+        /** @var User $userToUpdate */
+        $userToUpdate = $this->getStub()->offsetGet(0);
+
+        $userFixture = (new UserFixture())
+            ->getArray('Update Test');
+        unset($userFixture['password']);
+
+        $response = $this->put('/user/' . $userToUpdate->getId(), $userFixture);
+        $responsePayload = $response->json();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue($responsePayload['success']);
+        $this->assertNull($responsePayload['meta']);
+        $this->assertEquals($responsePayload['data']['name'], $userFixture['name']);
+        $this->assertEquals($responsePayload['data']['email'], $userFixture['email']);
+
+        /** @var User|boolean $userAfterUpdate */
+        $userAfterUpdate = User::findFirstById($responsePayload['data']['id']);
+        $this->assertNotFalse($userAfterUpdate);
+        $this->assertEquals($userFixture['name'], $userAfterUpdate->getName());
+        $this->assertEquals($userFixture['email'], $userAfterUpdate->getEmail());
+        $this->assertEquals($userToUpdate->getPassword(), $userAfterUpdate->getPassword());
+    }
+
     public function testNotFound() {
         $notFoundId = User::getNextId();
         $response = $this->put('/user/' . $notFoundId, []);
