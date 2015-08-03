bea8d5fba53b9f9160d4a77a79f63eb2ea7578a4
Alexey Novikov <alekseeey@gmail.com>
2015-08-03 20:40:26 +0300

    working on mapper

---
+++ rise/Mapper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rise;
+
+use \Phalcon\Annotations\Adapter\Memory;
+
+/**
+ * Class Mapper
+ * @package Rise
+ */
+class Mapper {
+
+    public function fromJson() {
+
+    }
+
+    public function fromArray() {
+
+    }
+
+    public function fromObject() {
+
+    }
+
+}
